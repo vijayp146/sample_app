0c4b9c8f6a74feb97b1a30bf0e846d1348377eca
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2012-10-21 14:21:28 -0400

    deploy setup working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,26 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "sample_app"
+set :repository, "git@github.com:AdamFerguson/sample_app.git"
+set :scm, :git
+set :user, "deploy"
+set :deploy_via, :remote_cache
+set :branch, "master"
+set :keep_releases, 5
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+default_run_options[:pty] = true
+set :use_sudo, false
+ssh_options[:forward_agent] = true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/var/rails/#{application}"
+
+# role :web, "aws.adam-ferguson.com"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+task :production do
+  set :rails_env, "production"
+  server 'aws.adam-ferguson.com', :app, :web, :db
+end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
